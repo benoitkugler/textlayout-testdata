79b12cd76cca6f9a70786a1143e43a26fdbe681d
Benoit KUGLER <x.ben.x@free.fr>
2022-05-06 15:36:31 +0200

    update .gitignore

---
+++ harfbuzz/data_test.go
@@ -0,0 +1,15 @@
+package harfbuzz
+
+import "testing"
+
+func TestLoadFile(t *testing.T) {
+	_, err := Files.Open("harfbuzz_reference/in-house/fonts/1a3d8f381387dd29be1e897e4b5100ac8b4829e1.ttf")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// _, err = Files.Open("harfbuzz_reference/in-house/macos/System/Library/Fonts/ProNW4.ttc")
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+}
